0b085b8ec6113e407c5e98cc9ae65ee97d5467e6
keerthana9944 <herald.keerthana@gmail.com>
2025-04-02 10:46:27 +0530

    updated

---
+++ src/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div style={{textAlign: "center", marginTop:"50px"}}>
+        <h1>Nukabatni Herald Keerthana</h1>
+        <p>Contact: nukabatni.herald.s86@kalvium.community</p>
+    </div>
+    );
+};
+
+export default Home;
